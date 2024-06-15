3c84226cc3b15a0300fc106c6e7754f931fc240f
Om Preetham <ompreethambandi@gmail.com>
2024-06-14 22:31:04 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Om Preetham Bandi on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
